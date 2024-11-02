70e5deee7d9403f68554853d5632229968db609e
Monicat263 <mda263@bol.com.br>
2024-11-02 04:35:00 -0300

    refatoração: teste unitários

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
     Faça um programa para ler os dados de um produto em estoque (nome, preço e quantidade no estoque) Em seguida:
         - Mostrar os dados do produto(nome, preço, quantidade no estoque, valor total no estoque)
         - Realiar um entrada no estoque e mostrar novamente os dados do produto
-        - Realizar uma saída no estoque e mostrar novamente os dados do produto
  */
 public class Main {
     public static void main(String[] args) {
